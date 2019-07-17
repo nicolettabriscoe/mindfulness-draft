0171c1fa2c10ad53ee9550f075d8e541935a88e2
Nicoletta Briscoe <nicolettabriscoe@Nicolettas-MacBook-Pro.local>
2019-07-16 21:39:23 -0700

    initial commit

---
+++ practiceMindful/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  practiceMindful
+//
+//  Created by Nicoletta Briscoe on 7/16/19.
+//  Copyright © 2019 Nicoletta Briscoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func oneTapped(_ sender: UIButton) {
+    }
+    
+}
+
